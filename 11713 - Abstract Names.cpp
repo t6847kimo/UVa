2793bbeddbd40af989eb9e35efb95edb2b27ee02
tychien <t6847kimo@gmail.com>
2016-01-01 13:10:40 +0800

    update format

--- 11713 - Abstract Names.cpp
+++ 11713 - Abstract Names.cpp
@@ -4,39 +4,52 @@
 #define SIZE 30
 
 bool inTarget(char in){
-     const char *target="aeiou";
-     for(int i=0;i<5;i++){
-             if(in==target[i])
-                              return true;
-     }
-     return false;
+	const char *target="aeiou";
+	for(int i=0;i<5;i++)
+	{
+		if(in==target[i]){
+			return true;
+		}
+	}
+	return false;
 }
+
 bool judge(char* a,char* b){
-     int aLen=strlen(a);
-     int bLen=strlen(b);
-     if(aLen!=bLen)
-                   return false;//
-     else{
-          for(int i=0;i<aLen;i++){
-                  if(a[i]!=b[i]&&(!inTarget(a[i])||!inTarget(b[i])))
-                                                          return false;
-          }
-          return true;
-     }
+	int aLen=strlen(a);
+	int bLen=strlen(b);
+	if(aLen!=bLen)
+	{
+		return false;//
+	}						 
+	else
+	{
+		for(int i=0;i<aLen;i++)
+		{
+			if(a[i]!=b[i]&&(!inTarget(a[i])||!inTarget(b[i])))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
+
 int main(){
-    int n;
-    char a[SIZE],b[SIZE];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-            scanf("%s",&a);
-            scanf("%s",&b);
-            if(judge(a,b))
-                         printf("Yes\n"); 
-            else
-                printf("No\n");
-    }
-    system("pause");
-    return 0;
-    
+	int n;
+	char a[SIZE],b[SIZE];
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s",&a);
+		scanf("%s",&b);
+		if(judge(a,b)){
+			printf("Yes\n"); 
+		}
+		else
+		{
+			printf("No\n");
+		}
+	}
+	system("pause");
+	return 0;
 }
